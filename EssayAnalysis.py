e971905421eb8f3369379261f7c51592b9ce0e9e
Dylan Lee <d.josephlee@gmail.com>
2021-07-31 21:56:14 -0700

    updates to scoring

--- EssayAnalysis.py
+++ EssayAnalysis.py
@@ -3,11 +3,14 @@ import statistics
 
 class Essay:
     def __init__(self):
-        self.crit_essay_scores = []
-        self.div_stat_scores = []
+        self.crit_essay_scores = [] #out of 100
+        self.div_stat_scores = [] #out of 20
         self.crit_score_agg = 0 #i.e. the mean of the critical essay crit_essay_scores
         self.div_score_agg = 0 #i.e. the mean of the diversity statement crit_essay_scores
-        self.editscore = 0
+        self.editscore = 0 #out of 435
+        self.perc_crit_essay_score = self.crit_score_agg/100 #crit essay score as percentage
+        self.perc_div_stat_score = self.div_score_agg/20
+        self.perc_edit_score = self.editscore/435
         self.finalscore = 0
 
 def strip_letters(string):
@@ -43,25 +46,33 @@ with open("Personal_Statements/Superseding_Responses_Div_Stat.csv", mode='r') as
         essay.div_stat_scores.append(float(row["Total Score"]))
         essay.div_score_agg = statistics.mean(essay.div_stat_scores)
 
-with open("2021 JJWC Edit Test Scores.csv", mode='r') as file
+with open("EditTestScores.csv", mode='r', encoding='utf-8-sig') as file:
     contents = csv.DictReader(file)
+    error_list = []
     for row in contents:
-        essay = d["%s" % strip_letters(row["ID"])]
-        essay.editscore = row["POINTS"]
+        try:
+            x = strip_letters(row["Competition#"])
+            essay = d["%s" % x]
+            essay.editscore = row["POINTS"]
+        except:
+            x = strip_letters(row["Competition#"])
+            error_list.append(x)
+
+print("Error: %s" % error_list)
 
 crit_essay_scores = []
 div_stat_scores = []
 edit_test_scores = []
 
 for item in d.keys():
-    crit_essay_scores.append(d[item].)
+    crit_essay_scores.append(d[item].crit_score_agg)
+    div_stat_scores.append(d[item].div_score_agg)
+    edit_test_scores.append(float(d[item].editscore))
 
 
 if __name__ == "__main__":
-    print(d['15'].crit_essay_scores)
-    print(d['15'].crit_score_agg)
-    print(d['15'].div_stat_scores)
-    print(strip_letters("Essay 147"))
+    print(edit_test_scores)
+
 
 
 
