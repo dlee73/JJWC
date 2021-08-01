23109701705560ac794075b49c2d9f6574c27842
Dylan Lee <d.josephlee@gmail.com>
2021-07-31 17:06:56 -0700

    initial

--- EssayAnalysis.py
+++ EssayAnalysis.py
@@ -1,2 +1,31 @@
+import csv
+import statistics
+
 class Essay:
-    def __init__(self, number=0, scores=[], ):
+    def __init__(self):
+        self.scores = []
+        self.div_stat_scores = []
+        self.score_agg = 0
+        self.div_score_agg = 0
+
+#Read the reader responses spreadsheet, save the data as a class Essay and key it to the essay number
+#save it to dictionary d.
+d = {}
+
+with open("Grader_responses.csv", mode="r") as file:
+    contents = csv.DictReader(file)
+
+    for row in contents:
+        if row["Competition Number"] not in d.keys():
+            d["%s" % row["Competition Number"]] = Essay()
+        essay = d["%s" % row["Competition Number"]]
+        essay.number = int(row["Competition Number"])
+        essay.scores.append(float(row["Total Score"]))
+        essay.score_agg = statistics.mean(essay.scores)
+
+print(d['1'].scores)
+print(d['1'].score_agg)
+
+
+
+
